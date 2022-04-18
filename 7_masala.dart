51e379e4954234167a3d09f6cb354bfc4c8c6336
Makhmudov Alijon <98184267+alijonmakhmudov@users.noreply.github.com>
2022-04-18 11:17:02 +0500

    Add files via upload

---
+++ 7_masala.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //  for (var i = 0; i <= a.length-1; i++) {
+  //    for (var j = 0; j < a.length-i-1; j++) {
+  //     if(a[j]>a[j+1]){
+  //       int temp=a[j];
+  //       a[j]=a[j+1];
+  //       a[j+1]=temp;
+  //     }
+  //    }
+}
+//savolga tushunmadim
